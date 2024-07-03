2ae52dc4c73172d1436040d20e7228521d162ead
Achintha Pallegedara <achinthahirudika@gmail.com>
2024-07-04 01:18:34 +0530

    🚔

---
+++ src/components/settings/dark-mode.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from "react";
+
+type Props = {};
+
+const DarkModetoggle = (props: Props) => {
+  return <div>DarkModetoggle</div>;
+};
+
+export default DarkModetoggle;
