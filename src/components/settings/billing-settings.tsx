6c6c90734931ba34d6683dc661a4c15287fe6d8b
Achintha Pallegedara <achinthahirudika@gmail.com>
2024-08-27 15:13:06 +0530

    feat: Add Accordion component for collapsible content

--- src/components/settings/billing-settings.tsx
+++ src/components/settings/billing-settings.tsx
@@ -3,19 +3,19 @@ import React from "react";
 import Section from "../section-label";
 import { Card, CardContent, CardDescription } from "../ui/card";
 import { Check, CheckCircle2, Plus } from "lucide-react";
-// import { pricingCards } from '@/constants/landing-page'
-// import Modal from '../mondal'
+import { pricingCards } from "@/constants/landing-page";
 // import SubscriptionForm from '../forms/settings/subscription-form'
 import Image from "next/image";
+import Modal from "../mondal";
 
 type Props = {};
 
 const BillingSettings = async (props: Props) => {
   const plan = await onGetSubscriptionPlan();
-  // const planFeatures = pricingCards.find(
-  //   (card) => card.title.toUpperCase() === plan?.toUpperCase()
-  // )?.features
-  // if (!planFeatures) return
+  const planFeatures = pricingCards.find(
+    (card) => card.title.toUpperCase() === plan?.toUpperCase()
+  )?.features;
+  if (!planFeatures) return;
 
   // console.log(planFeatures)
   return (
@@ -27,11 +27,11 @@ const BillingSettings = async (props: Props) => {
         />
       </div>
       <div className="lg:col-span-2 flex justify-start lg:justify-center ">
-        {/* <Modal
+        <Modal
           title="Choose A Plan"
           description="Tell us about yourself! What do you do? Let’s tailor your experience so it best suits you."
           trigger={
-            plan && plan === 'STANDARD' ? (
+            plan && plan === "STANDARD" ? (
               <Card className="border-dashed bg-cream border-gray-400 w-full cursor-pointer h-[270px] flex justify-center items-center">
                 <CardContent className="flex gap-2 items-center">
                   <div className="rounded-full border-2 p-1">
@@ -52,22 +52,20 @@ const BillingSettings = async (props: Props) => {
             )
           }
         >
-          <SubscriptionForm plan={plan!} />
-        </Modal> */}
+          {/* <SubscriptionForm plan={plan!} /> */}
+          hello
+        </Modal>
       </div>
       <div className="lg:col-span-2">
         <h3 className="text-xl font-semibold mb-2">Current Plan</h3>
         <p className="text-sm font-semibold">{plan}</p>
         <div className="flex gap-2 flex-col mt-2">
-          {/* {planFeatures.map((feature) => (
-            <div
-              key={feature}
-              className="flex gap-2"
-            >
+          {planFeatures.map((feature) => (
+            <div key={feature} className="flex gap-2">
               <CheckCircle2 className="text-muted-foreground" />
               <p className="text-muted-foreground">{feature}</p>
             </div>
-          ))} */}
+          ))}
         </div>
       </div>
     </div>
